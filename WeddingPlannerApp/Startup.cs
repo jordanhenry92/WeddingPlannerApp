ac405a8818ce4fa170d7e15868f1746677bb5ffe
Jordan Henry <jordanhenry92@hotmail.com>
2020-08-06 23:40:32 -0400

    Add project files.

---
+++ WeddingPlannerApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeddingPlannerApp.Startup))]
+namespace WeddingPlannerApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

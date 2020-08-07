0754e1acb599de5517250e867c30ede9d848754c
Jordan Henry <jordanhenry92@hotmail.com>
2020-08-06 23:55:57 -0400

    Added bootstrap style

--- WeddingPlannerApp/App_Start/BundleConfig.cs
+++ WeddingPlannerApp/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace WeddingPlannerApp
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lux.css",
                       "~/Content/site.css"));
         }
     }
